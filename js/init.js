a8b64df924ce645a3d7e6e1a6dafb0d9def3cea0
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2024-08-17 16:52:22 -0300

    alterações no css

--- js/init.js
+++ js/init.js
@@ -4,5 +4,11 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    // Init click event for the list items
+    $('ul li').click(function(){
+      $('li ').removeClass("active");
+      $(this).addClass("active");
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
